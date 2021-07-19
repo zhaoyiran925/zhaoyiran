a84fffe9a798e4b64f84ee2ef8672ed03129c6e0
zhaoss <chinazss@126.com>
2021-07-19 21:55:42 +0800

    更新了Employee.java

--- GitEmploy/src/cn/mldn1/Employee.java
+++ GitEmploy/src/cn/mldn1/Employee.java
@@ -3,4 +3,28 @@ package cn.mldn1;
 public class Employee {
     private String name;
     private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
